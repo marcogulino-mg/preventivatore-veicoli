481d4ebf087b348c34f0c45a0b603a3c1c952c9d
marcogulino-mg <marcogulino98@gmail.com>
2025-11-27 16:05:23 +0100

    first commit

---
+++ src/main/java/com/marcogulino/preventivatore/preventivatore_veicoli/PreventivatoreVeicoliApplication.java
@@ -0,0 +1,13 @@
+package com.marcogulino.preventivatore.preventivatore_veicoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreventivatoreVeicoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreventivatoreVeicoliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcogulino/preventivatore/preventivatore_veicoli/PreventivatoreVeicoliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcogulino.preventivatore.preventivatore_veicoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreventivatoreVeicoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
